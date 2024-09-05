734faf40a64c288d70831c4ef64ace307c762fb4
athenaligit <156002873+athenaligit@users.noreply.github.com>
2024-09-05 13:46:51 -0400

    4

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
-message = 'Hello world'
-message.replace('world', 'universe')
-print(message)
+names = ['James', 'Tom', 'Sam']
+for index, name in enumerate(names, start=1):
+    print(index, name)
+
+def summ(a,b):
+    return a+b
+print(summ(4,3))
